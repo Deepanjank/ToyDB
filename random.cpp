5909f5f4e3d092e9097aed389327261441af3085
Roshan <thisisroshansmail@gmail.com>
2014-11-24 15:34:04 +0530

    Test files

--- random.cpp
+++ random.cpp
@@ -4,10 +4,10 @@ int main()
 {
 	int t=0;
 	srand(0);
-	cout<<200<<endl;
+	cout<<300<<endl;
 	for(int i=0;i<200;i++)
 	{
-		cout<<0<<" "<<rand()%2<<" "<<rand()%200<<" "<<t<<endl;
+		if(i<100) cout<<0<<" "<<rand()%2<<" "<<rand()%200<<" "<<t<<endl;
 		if(i<200) cout<<1<<" "<<0<<" "<<i<<" "<<t<<endl;
 		t+=(rand()%3 > 1)*5;
 	}
